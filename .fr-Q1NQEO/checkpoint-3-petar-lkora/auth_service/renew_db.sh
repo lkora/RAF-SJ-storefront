7474bb9c9ab9f8709929a15c363de7e76ad16d2f
Luka Korica 81/2022 RN <lkorica8122rn@raf.rs>
2024-01-10 21:20:02 +0100

    Removed the .fr-Q1NQE0 link to the same repo

--- .fr-Q1NQEO/checkpoint-3-petar-lkora/auth_service/renew_db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-node_modules/sequelize-cli/lib/sequelize db:migrate:undo:all
-node_modules/sequelize-cli/lib/sequelize db:migrate
-node_modules/sequelize-cli/lib/sequelize db:seed:all
--- .fr-Q1NQEO/checkpoint-3-petar-lkora/auth_service/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { loginRouter } from './login'
-import { registerRouter } from './register'
-
-export const router = Router()
-
-router.use('/login', loginRouter)
-router.use('/register', registerRouter)
